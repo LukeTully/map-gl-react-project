df7817586723bff10cd115976081b95a2b2339ea
Luke <martinlm@umanitoba.ca>
2018-11-22 20:57:49 -0800

    Implement basic List component

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
-
+import ListSubGroup from './ListSubGroup';
 import styles from './List.scss';
 
 export default function List(props) {
+  const items = props.items;
+  const sites = props.sites;
+  const list = items.map((item, index) => {
+    if (item.sites.length > 0) {
+      return (
+        <ListSubGroup key={index} sites={sites} item={item}></ListSubGroup>
+      );
+    }
+  });
   return (
-    <div></div>
+    <div>
+      {list}
+    </div>
   );
 }
