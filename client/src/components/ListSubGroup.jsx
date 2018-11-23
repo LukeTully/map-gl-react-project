451a0d217f78618bce4f24a1a0c07eef88dfcdf6
Luke <martinlm@umanitoba.ca>
2018-11-23 03:38:43 -0800

    Implement site selection

--- client/src/components/ListSubGroup.jsx
+++ client/src/components/ListSubGroup.jsx
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import styles from './ListSubGroup.scss';
-import { connect } from 'react-redux';
 import classNames from 'classnames';
 
+/* Component for rendering sites within the project list menu */
 class ListSubGroup extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showing: false
+      showing: false, // is this current subgroup expanded or not
+      selected: 0
     };
-
 
     this.toggleShowing = this.toggleShowing.bind(this);
   }
+  /* Select a new site */
+  selectSite(id, e) {
+    this.props.selectSite(id);
+  }
 
+  /* Change the expanded local state of this subgroup */
   toggleShowing() {
     this.setState((state, props) => {
       return state.showing ? { showing: false } : { showing: true };
@@ -23,16 +28,29 @@ class ListSubGroup extends Component {
   render() {
     const sites = this.props.sites;
     const item = this.props.item;
+
+    /* Define a conditional class to control visibility on this subgroup */
     const listSubGroupClasses = classNames({
       [styles.show]: this.state.showing
     });
+
     return (
       <div className={listSubGroupClasses}>
         {this.state.showing}
-        <h2 className={styles.list_heading} onClick={this.toggleShowing}>{item.name}</h2>
+        <h2 className={styles.list_heading} onClick={this.toggleShowing}>
+          {item.name}
+        </h2>
         <ul className={styles.list}>
           {item.sites.map((site, index) => {
-            return <li className={styles.list_item} key={index}>{sites["byId"][site].name}</li>;
+            return (
+              <li
+                className={styles.list_item}
+                key={index}
+                onClick={this.selectSite.bind(this, site)}
+              >
+                {sites[site].name}
+              </li>
+            );
           })}
         </ul>
       </div>
@@ -40,10 +58,4 @@ class ListSubGroup extends Component {
   }
 }
 
-function mapStateToProps (state) {
-  return {
-    sites: state.sites
-  }
-}
-
-export default connect(mapStateToProps)(ListSubGroup);
+export default ListSubGroup;
