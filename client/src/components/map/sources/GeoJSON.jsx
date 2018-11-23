6ac79b13465efe5b7e0660729f3a0ea1431f4d99
Luke <martinlm@umanitoba.ca>
2018-11-23 03:41:42 -0800

    Add high level comp description

--- client/src/components/map/sources/GeoJSON.jsx
+++ client/src/components/map/sources/GeoJSON.jsx
@@ -2,19 +2,25 @@ import React, { Component } from 'react';
 
 import MapContext from '../context';
 
+/* Component for managing map sources */
 class GeoJSON extends Component {
   removeSource(id) {
     const { map } = this.props;
     const source = map.getSource(id);
 
-    if(source) {
+    if (source) {
       const order = map.getStyle().layers;
-      const layers = order.map((layer, index) => {
-        return { ...layer, before: (order[index + 1] && order[index + 1].id) || undefined }
-      }).filter(layer => layer.source === source.id);
-      
+      const layers = order
+        .map((layer, index) => {
+          return {
+            ...layer,
+            before: (order[index + 1] && order[index + 1].id) || undefined
+          };
+        })
+        .filter(layer => layer.source === source.id);
+
       layers.forEach(layer => {
-        if(map.getLayer(layer.id)) {
+        if (map.getLayer(layer.id)) {
           map.removeLayer(layer.id);
         }
       });
@@ -26,11 +32,10 @@ class GeoJSON extends Component {
   componentDidUpdate(prevProps, prevState) {
     const { map, data, id } = this.props;
 
-    if(map) {
-      if(map.getSource(id)) {
+    if (map) {
+      if (map.getSource(id)) {
         this.removeSource(id);
       }
-      
       map.addSource(id, { type: 'geojson', data });
     }
   }
@@ -38,14 +43,14 @@ class GeoJSON extends Component {
   componentWillUnmount() {
     this.props.map.removeSource(this.props.id);
   }
-  
+
   render() {
     return null;
-  } 
+  }
 }
 
 export default props => (
   <MapContext.Consumer>
-    { context => <GeoJSON { ...props } { ...context } /> }
+    {context => <GeoJSON {...props} {...context} />}
   </MapContext.Consumer>
 );
